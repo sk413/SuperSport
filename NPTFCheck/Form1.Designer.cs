3694adfbf893d9832f735ac7ddb50984f4acb625
Alvin S <99166784+sk413@users.noreply.github.com>
2022-02-10 03:36:53 -0800

    Files Upload

---
+++ NPTFCheck/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace NPTFCheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.chkontop = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(165, 170);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create Report";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 137);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 11);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "0";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(55, 11);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "/";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(87, 11);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 29);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "0";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Verdana", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(55, 64);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(383, 28);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "NPTF Text finder in Drawing ";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::NPTFCheck.Properties.Resources.esskay;
+            this.pictureBox2.Location = new System.Drawing.Point(446, 11);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(105, 155);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // chkontop
+            // 
+            this.chkontop.AutoSize = true;
+            this.chkontop.Location = new System.Drawing.Point(459, 254);
+            this.chkontop.Name = "chkontop";
+            this.chkontop.Size = new System.Drawing.Size(74, 20);
+            this.chkontop.TabIndex = 10;
+            this.chkontop.Text = "On Top";
+            this.chkontop.UseVisualStyleBackColor = true;
+            this.chkontop.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 298);
+            this.Controls.Add(this.chkontop);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Esskay In House Tool";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.CheckBox chkontop;
+    }
+}
+
---
+++ NPTFCheck/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tekla.Structures.Drawing;
+using Tekla.Structures.Model;
+using Part = Tekla.Structures.Model.Part;
+
+namespace NPTFCheck
+{
+    public partial class Form1 : Form
+    {
+        public static string skApplicationName = "NPTF Text finder in Drawing ";
+        public static string skApplicationVersion = "2201.02";  
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            skWinLib.accesslog(skApplicationName, skApplicationVersion, "Create Report", "");
+            int ct = 0;
+
+            DrawingHandler drawingHandler = new DrawingHandler();
+            List<string> missingNPTF = new List<string>();
+            List<string> UnwantedNPTF = new List<string>();
+            List<string> catchFile = new List<string>();
+            DrawingEnumerator drawingEnumerator = drawingHandler.GetDrawingSelector().GetSelected();
+            label1.Text = "Processing.........";
+            label1.Visible = true;
+            int scount = drawingEnumerator.GetSize();
+            int a = 0;
+            label4.Text = scount.ToString();
+            label4.Visible = true;
+            label2.Visible = true;
+            while(drawingEnumerator.MoveNext())
+            {
+                ct++;
+                AssemblyDrawing assemblyDrawing = drawingEnumerator.Current as AssemblyDrawing;
+                try
+                {
+                    if (assemblyDrawing != null)
+                    {
+                        Tekla.Structures.Model.ModelObject modelObject = new Model().SelectModelObject(assemblyDrawing.AssemblyIdentifier);
+                        Assembly assembly = modelObject as Assembly;
+                        Tekla.Structures.Model.Part part = assembly.GetMainPart() as Part;
+                        string studs = "";
+                        part.GetUserProperty("USER_FIELD_1", ref studs);
+                        if(studs.Trim(' ') =="")
+                        {
+                            int stud = 0;
+                            part.GetUserProperty("SHEARSTUDS", ref stud);
+                            studs = stud.ToString();
+
+                        }
+                        DrawingObjectEnumerator drawingObjectEnumerator = assemblyDrawing.GetSheet().GetAllObjects(typeof(Text));
+                        bool containsPaint = false;
+                        while (drawingObjectEnumerator.MoveNext())
+                        {
+                            ct++;
+                            Text text = drawingObjectEnumerator.Current as Text;
+                            string Mytext = text.TextString.ToUpper().Trim().Replace(" ","");
+                                if (Mytext.Equals("NOPAINT\n@TOPFLG."))
+                                {
+                                    containsPaint = true;
+                                    break;
+                                }
+                                else if (Mytext.Equals("NPTF"))
+                                {
+                                    containsPaint = true;
+                                    break;
+                                }
+
+                        }
+                        if ((studs !="0") && (containsPaint == false))
+                        {
+                            missingNPTF.Add(assemblyDrawing.Mark);
+                        }
+                        else if ((studs =="0") && (containsPaint == true))
+                        {
+                            UnwantedNPTF.Add(assemblyDrawing.Mark);
+                        }
+                    }
+                }
+                catch
+                {
+                    catchFile.Add(assemblyDrawing.Mark);
+                }
+                a++;
+
+
+                label2.Text = a.ToString();
+            }
+
+            createReport(missingNPTF,"MissingNPTF");
+            createReport(catchFile, "CatchFile");
+            createReport(UnwantedNPTF, "UnwantedNPTF");
+            label1.Visible = false;
+            MessageBox.Show("Completed");
+
+            skWinLib.worklog(skApplicationName, skApplicationVersion, "Create Report", ";Total:" + ct.ToString());
+        }
+        private void createReport(List<string> drawingNumber,string fileName)
+        {
+            Model model = new Model();
+            ModelInfo modelinfo = model.GetInfo();
+            System.IO.Directory.CreateDirectory(modelinfo.ModelPath + "\\Automation");
+            String reportPath = modelinfo.ModelPath + "\\Automation\\"+fileName+".csv";
+            StreamWriter sw = new StreamWriter(reportPath, false);
+            foreach (string reqInfo in drawingNumber)
+            {
+                sw.Write(reqInfo);
+                sw.WriteLine();
+            }
+            sw.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.TopMost = chkontop.Checked;
+        }
+    }
+}
